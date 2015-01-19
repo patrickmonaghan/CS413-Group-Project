42ab5c634dc0a571a9134e6da8af1ed2e912967c
Robert Rendell <rob_452@hotmail.co.uk>
2015-01-19 21:39:32 +0000

    made real time gui update every half second, also randomises values to give an idea of what it will look like. started on json loading code

--- realtime-front-end/realtimegui.py
+++ realtime-front-end/realtimegui.py
@@ -5,6 +5,9 @@
 #    Jan 16, 2015 06:41:20 PM
 import sys
 import threading 
+import json
+
+from datetime import datetime, timedelta
 
 from random import randint
 
@@ -33,6 +36,8 @@ def vp_start_gui():
     root.mainloop()
 
 w = None
+jsonfile = "gui.json"
+
 def create_Real_time_display(root, param=None):
     '''Starting point when module is imported by another program.'''
     global w, w_win, rt
@@ -53,7 +58,19 @@ class Real_time_display:
     
     def __init__(self, master=None):
         
-        global temperatures
+        self.temperatures_shown = False
+        self.count = 0
+        self.journeyTime = 0
+        self.gpsLAT = 0.0
+        self.gpsLONG = 0.0
+        self.mph = 0 
+        self.mpg = 0
+        self.revs = 0
+        self.miles = 0
+        self.throttle = 0
+        self.load = 0
+        self.engine_temp = 0
+        self.air_temp = 0
         
         _bgcolor = '#d9d9d9'  # X11 color: 'gray85'
         _fgcolor = '#000000'  # X11 color: 'black'
@@ -71,29 +88,17 @@ class Real_time_display:
         master.configure(highlightcolor="black")
 
 
-        self.lblSpeed = Label(master)
-        self.lblSpeed.place(relx=0.05, rely=0.17, height=140, width=520)
-        self.lblSpeed.configure(activebackground="#f9f9f9")
-        self.lblSpeed.configure(activeforeground="black")
-        self.lblSpeed.configure(background="#ffffff")
-        self.lblSpeed.configure(disabledforeground="#a3a3a3")
-        self.lblSpeed.configure(font=font17)
-        self.lblSpeed.configure(foreground="#000000")
-        self.lblSpeed.configure(highlightbackground="#d9d9d9")
-        self.lblSpeed.configure(highlightcolor="black")
-        self.lblSpeed.configure(text='''25MPH''')
-
-        self.lblinfo1 = Label(master)
-        self.lblinfo1.place(relx=0.05, rely=0.35, height=55, width=520)
-        self.lblinfo1.configure(activebackground="#f9f9f9")
-        self.lblinfo1.configure(activeforeground="black")
-        self.lblinfo1.configure(background="#ffffff")
-        self.lblinfo1.configure(disabledforeground="#a3a3a3")
-        self.lblinfo1.configure(font=font16)
-        self.lblinfo1.configure(foreground="#000000")
-        self.lblinfo1.configure(highlightbackground="#d9d9d9")
-        self.lblinfo1.configure(highlightcolor="black")
-        self.lblinfo1.configure(text='''speed''')
+        self.lblMPH = Label(master)
+        self.lblMPH.place(relx=0.05, rely=0.17, height=140, width=520)
+        self.lblMPH.configure(activebackground="#f9f9f9")
+        self.lblMPH.configure(activeforeground="black")
+        self.lblMPH.configure(background="#ffffff")
+        self.lblMPH.configure(disabledforeground="#a3a3a3")
+        self.lblMPH.configure(font=font17)
+        self.lblMPH.configure(foreground="#000000")
+        self.lblMPH.configure(highlightbackground="#d9d9d9")
+        self.lblMPH.configure(highlightcolor="black")
+        self.lblMPH.configure(text='''25mph''')
 
         self.menubar = Menu(master,bg=_bgcolor,fg=_fgcolor)
         master.configure(menu = self.menubar)
@@ -157,19 +162,8 @@ class Real_time_display:
         self.lblMPG.configure(foreground="#000000")
         self.lblMPG.configure(highlightbackground="#d9d9d9")
         self.lblMPG.configure(highlightcolor="black")
-        self.lblMPG.configure(text='''40MPG''')
+        self.lblMPG.configure(text='''40mpg''')
 
-        self.lblNameConsumption = Label(master)
-        self.lblNameConsumption.place(relx=0.57, rely=0.35, height=55, width=520)
-        self.lblNameConsumption.configure(activebackground="#f9f9f9")
-        self.lblNameConsumption.configure(activeforeground="black")
-        self.lblNameConsumption.configure(background="#ffffff")
-        self.lblNameConsumption.configure(disabledforeground="#a3a3a3")
-        self.lblNameConsumption.configure(font=font16)
-        self.lblNameConsumption.configure(foreground="#000000")
-        self.lblNameConsumption.configure(highlightbackground="#d9d9d9")
-        self.lblNameConsumption.configure(highlightcolor="black")
-        self.lblNameConsumption.configure(text='''consumption''')
 
         self.lblRPM = Label(master)
         self.lblRPM.place(relx=0.24, rely=0.45, height=140, width=680)
@@ -181,19 +175,7 @@ class Real_time_display:
         self.lblRPM.configure(foreground="#000000")
         self.lblRPM.configure(highlightbackground="#d9d9d9")
         self.lblRPM.configure(highlightcolor="black")
-        self.lblRPM.configure(text='''1000RPM''')
-
-        self.lblNameRevs = Label(master)
-        self.lblNameRevs.place(relx=0.35, rely=0.66, height=33, width=380)
-        self.lblNameRevs.configure(activebackground="#f9f9f9")
-        self.lblNameRevs.configure(activeforeground="black")
-        self.lblNameRevs.configure(background="#ffffff")
-        self.lblNameRevs.configure(disabledforeground="#a3a3a3")
-        self.lblNameRevs.configure(font=font16)
-        self.lblNameRevs.configure(foreground="#000000")
-        self.lblNameRevs.configure(highlightbackground="#d9d9d9")
-        self.lblNameRevs.configure(highlightcolor="black")
-        self.lblNameRevs.configure(text='''revs''')
+        self.lblRPM.configure(text='''1000rpm''')
 
         self.lblScroller1 = Label(master)
         self.lblScroller1.place(relx=0.01, rely=0.79, height=53, width=406)
@@ -267,30 +249,86 @@ class Real_time_display:
         self.lblScrollerVal2.configure(highlightcolor="black")
         self.lblScrollerVal2.configure(text='''7''')
 
-        temperatures = False 
-        self.startTimer(5.0)
+        self.temperatures_shown = False 
+        self.timeoutRead()
+        self.timeoutSwitch()
         
     def switchLabels(self):
-        global temperatures
-        if(temperatures):
+        #global temperatures_shown
+        self.temperatures_shown = not self.temperatures_shown
+        self.updateView()
+        self.timeoutSwitch(5.0)
+        
+    def randomValues(self):
+#       global temperatures_shown, journeyTime, gpsLAT, gpsLONG, mph, mpg, revs, miles, throttle, load, engine_temp, air_temp, scrollVal1, scrollVal2
+        self.count += 1
+        if(self.count % 2 == 0):
+            self.journeyTime += 1
+        self.gpsLAT = randint(0.0,90.0)
+        self.gpsLONG = randint(0.0,180.0)
+        self.mph = randint(30,40)
+        self.mpg = randint(40,50)
+        self.revs = randint(2000,3000)
+        self.miles += 0.1
+        self.throttle += 1
+        if(self.throttle > 20):
+            self.throttle = 0
+        self.engine_load = randint(100,200)
+        self.engine_temp = randint(100,130)
+        self.air_temp = randint(9,10)
+        
+        
+        self.updateView()
+        self.timeoutRead()
+        
+    def readValues(self):
+        global jsonfile
+        currentValues = json.load(jsonfile)
+        
+        self.count += 1
+        if(self.count % 2 == 0):
+            self.journeyTime += 1
+        self.gpsLAT = randint(0.0,90.0)
+        self.gpsLONG = randint(0.0,180.0)
+        self.mph = randint(30,40)
+        self.mpg = randint(40,50)
+        self.revs = randint(2000,3000)
+        self.miles += 0.1
+        self.throttle += 1
+        if(self.throttle > 20):
+            self.throttle = 0
+        self.engine_load = randint(100,200)
+        self.engine_temp = randint(100,130)
+        self.air_temp = randint(9,10)
+        
+    def updateView(self):
+        #global temperatures_shown, journeyTime, gpsLAT, gpsLONG, mph, mpg, revs, miles, throttle, load, engine_temp, air_temp
+        sec = timedelta(seconds=int(self.journeyTime))
+        d = datetime(1,1,1) + sec
+    
+        self.lblTime.configure(text=str(d.hour) + "h " + str(d.minute) + "m " + str(d.second) + "s")
+        self.lblGPS.configure(text=str(self.gpsLAT) + "N" + str(self.gpsLONG) + "E")
+        self.lblMilesVal.configure(text=str(self.miles) + "m")
+        self.lblMPH.configure(text=str(self.mph) + "mph")
+        self.lblMPG.configure(text=str(self.mpg) + "mpg")
+        self.lblRPM.configure(text=str(self.revs) + "rpm")
+        if(self.temperatures_shown):
             self.lblScroller1.configure(text='''Engine''')
-            self.lblScrollerVal1.configure(text='''137oC''')
+            self.lblScrollerVal1.configure(text=str(self.engine_temp) + u"\u00B0" + "C")
             self.lblScroller2.configure(text='''Air''')
-            self.lblScrollerVal2.configure(text='''7oC''')
-            temperatures = False
-
+            self.lblScrollerVal2.configure(text=str(self.air_temp) + u"\u00B0" + "C")
         else:
             self.lblScroller1.configure(text='''Load''')
-            self.lblScrollerVal1.configure(text='''200''')
+            self.lblScrollerVal1.configure(text=str(self.engine_load))
             self.lblScroller2.configure(text='''Throttle''')
-            self.lblScrollerVal2.configure(text='''30''')
-            temperatures = True
+            self.lblScrollerVal2.configure(text=str(self.throttle) + "%")
+        
+    def timeoutSwitch(self):
+        threading.Timer(5.0, self.switchLabels).start()
             
-        self.startTimer(5.0)
-        
-    def startTimer(self, time):
-        
-            threading.Timer(time, self.switchLabels).start()
+    def timeoutRead(self):
+
+        threading.Timer(0.5, self.randomValues).start()
         
 if __name__ == '__main__':
     vp_start_gui()
